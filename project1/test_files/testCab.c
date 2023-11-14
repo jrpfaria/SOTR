d19093aba1713ee3942a8e391714ee16225ce361
rodrigofaria <jrfaria@ua.pt>
2023-11-14 00:50:02 +0000

    better test cab

--- project1/test_files/testCab.c
+++ project1/test_files/testCab.c
@@ -2,67 +2,58 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-#include <pthread.h>
-#include "./include/cab.h"
+#include "../include/cab.h"
 
 static int k = 0, j = 0;
-static char *names[10] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"}; 
-static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+static const char *testData[] = {"Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
 static CAB_BUFFER* top;
 
 // Dummy task function for testing
 void* dummy_task(void* arg) {
-    pthread_mutex_lock(&mutex);
     CAB* c = (CAB*)arg;
 
     fprintf(stderr, "Free: %p\n", c->free);
     fprintf(stderr, "MRB: %p\n", c->mrb);
 
-    if (c->free->next == NULL)
+    if (c->mrb->next == NULL)
         unget(c, &top[j++]);
     if (j >= 3)
         j = 0;
 
     CAB_BUFFER* buffer = reserve(c);
-    char* new_data = malloc(sizeof(char));
+    char* new_data = malloc(c->buffer_size);
 
-    // Perform some task
-    char* data = malloc(sizeof(char));
+    char* data = malloc(c->buffer_size);
     
-    if (k<10){
-        *new_data = names[k++][0];
+    if (k < 10) {
+        strcpy(new_data, testData[k++]);
     } else {
-        *new_data = 'Z';
+        strcpy(new_data, "Z");
     }
 
-    memcpy(data, new_data, sizeof(char));
+    memcpy(data, new_data, c->buffer_size);
 
     // Put the result in the CAB
     put_mes(c, buffer, data);
-    fprintf(stderr, "Data in MRB: %c\n", *((char*)get_mes(c)));
+    fprintf(stderr, "Data in MRB: %s\n", (char*)get_mes(c));
 
-    free(data);
-    pthread_mutex_unlock(&mutex);
+    fprintf(stderr, "Data in CAB BUFFERS: ");
+    for (int t = 0; t < 6; ++t)
+        if (top[t].data != NULL)
+            fprintf(stderr, "%s, ", ((char*)top[t].data));
+    fprintf(stderr, "\n");
 
     return NULL;
 }
 
 int main() {
-    // Create a CAB with a buffer size of 128 and 5 tasks
-    CAB* myCab = open_cab(sizeof(char), 5);
+    // Create a CAB with a buffer size of 20 chars and 5 tasks
+    CAB* myCab = open_cab((sizeof(char) * 20), 5);
     top = &myCab->mrb[0];
-
-    // Create an array of threads to simulate tasks
-    pthread_t threads[10];
 
     // Create threads and perform tasks
     for (int i = 0; i < 10; ++i) {
-        pthread_create(&threads[i], NULL, dummy_task, (void*)myCab);
-    }
-
-    // Wait for threads to finish
-    for (int i = 0; i < 10; ++i) {
-        pthread_join(threads[i], NULL);
+        dummy_task((void*)myCab);
     }
 
     // Clean up
