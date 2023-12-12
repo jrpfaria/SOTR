f2a69901879039229debeeb93d07cb456e3a8a69
LAPTOP-M8283F5H\nunom <nunomsousa@ua.pt>
2023-12-12 12:32:07 +0000

    implementation of threads

--- project2/src/main.c
+++ project2/src/main.c
@@ -25,6 +25,7 @@
 #include <zephyr/drivers/uart.h>    /* for ADC API*/
 #include <zephyr/sys/printk.h>      /* for printk()*/
 #include <stdio.h>                  /* for sprintf() */
+#include <zephyr/timing/timing.h>   /* Kernel timing services */
 #include <zephyr/sys/sem.h>
 #include <stdlib.h>
 #include <string.h>
@@ -38,6 +39,51 @@
 #define RXBUF_SIZE 60                   /* RX buffer size */
 #define TXBUF_SIZE 60                   /* TX buffer size */
 #define RX_TIMEOUT 1000                 /* Inactivity period after the instant when last char was received that triggers an rx event (in us) */
+
+/* Size of stack area used by each thread (can be thread specific, if necessary)*/
+#define STACK_SIZE 1024
+
+/* Thread scheduling priority */
+#define UART_prio 1
+#define LEDS_prio 1
+#define BTNS_prio 1
+#define TC74_prio 1
+
+/* Thread periodicity (in ms)*/
+#define UART_period 1000
+#define LEDS_period 2000
+#define BTNS_period 4000
+#define TC74_period 4000
+
+/* Create thread stack space */
+K_THREAD_STACK_DEFINE(UART_stack, STACK_SIZE);
+K_THREAD_STACK_DEFINE(LEDS_stack, STACK_SIZE);
+K_THREAD_STACK_DEFINE(BTNS_stack, STACK_SIZE);
+K_THREAD_STACK_DEFINE(TC74_stack, STACK_SIZE);
+  
+/* Create variables for thread data */
+struct k_thread UART_data;
+struct k_thread LEDS_data;
+struct k_thread BTNS_data;
+struct k_thread TC74_data;
+
+/* Create task IDs */
+k_tid_t UART_tid;
+k_tid_t LEDS_tid;
+k_tid_t BTNS_tid;
+k_tid_t TC74_tid;
+
+/* Thread code prototypes */
+void UART_code(void *argA, void *argB, void *argC);
+void LEDS_code(void *argA, void *argB, void *argC);
+void BTNS_code(void *argA, void *argB, void *argC);
+void TC74_code(void *argA, void *argB, void *argC);
+
+/* Define timers for tasks activations */
+K_TIMER_DEFINE(UART_timer, NULL, NULL);
+K_TIMER_DEFINE(LEDS_timer, NULL, NULL);
+K_TIMER_DEFINE(BTNS_timer, NULL, NULL);
+K_TIMER_DEFINE(TC74_timer, NULL, NULL);
 
 /* Struct for UART configuration (if using default values is not needed) */
 const struct uart_config uart_cfg = {
@@ -62,7 +108,71 @@ static void uart_cb(const struct device *dev, struct uart_event *evt, void *user
 /* Main function */
 void main(void)
 {
-    
+    UART_tid = k_thread_create(&UART_data, UART_stack,
+        K_THREAD_STACK_SIZEOF(UART_stack), UART_code,
+        NULL,NULL,NULL, UART_prio, 0, K_NO_WAIT);
+ 
+}
+
+
+/* UART callback implementation */
+/* Note that callback functions are executed in the scope of interrupt handlers. */
+/* They run asynchronously after hardware/software interrupts and have a higher priority than all threads */
+/* Should be kept as short and simple as possible. Heavier processing should be deferred to a task with suitable priority*/
+static void uart_cb(const struct device *dev, struct uart_event *evt, void *user_data)
+{
+    int err;
+
+    switch (evt->type) {
+	
+        case UART_TX_DONE:
+		    printk("UART_TX_DONE event \n\r");
+            k_sem_give(&uart_sem);
+            break;
+
+    	case UART_TX_ABORTED:
+	    	printk("UART_TX_ABORTED event \n\r");
+		    break;
+		
+	    case UART_RX_RDY:
+		    // printk("UART_RX_RDY event \n\r");
+            /* Just copy data to a buffer. Usually it is preferable to use e.g. a FIFO to communicate with a task that shall process the messages*/
+            memcpy(&rx_chars[uart_rxbuf_nchar],&(rx_buf[evt->data.rx.offset]),evt->data.rx.len);
+            uart_rxbuf_nchar++;
+		    break;
+
+	    case UART_RX_BUF_REQUEST:
+		    printk("UART_RX_BUF_REQUEST event \n\r");
+		    break;
+
+	    case UART_RX_BUF_RELEASED:
+		    printk("UART_RX_BUF_RELEASED event \n\r");
+		    break;
+		
+	    case UART_RX_DISABLED: 
+            /* When the RX_BUFF becomes full RX is is disabled automaticaly.  */
+            /* It must be re-enabled manually for continuous reception */
+            printk("UART_RX_DISABLED event \n\r");
+		    err =  uart_rx_enable(uart_dev ,rx_buf,sizeof(rx_buf),RX_TIMEOUT);
+            if (err) {
+                printk("uart_rx_enable() error. Error code:%d\n\r",err);
+                exit(FATAL_ERR);                
+            }
+		    break;
+
+	    case UART_RX_STOPPED:
+		    printk("UART_RX_STOPPED event \n\r");
+		    break;
+		
+	    default:
+            printk("UART: unknown event \n\r");
+		    break;
+    }
+
+}
+
+void UART_code(void *argA, void *argB, void *argC){
+
     /* Local vars */    
     int err=0; /* Generic error variable */
     uint8_t welcome_mesg[] = "UART demo: Type a few chars in a row and then pause for a little while ...\n\r"; 
@@ -204,62 +314,7 @@ void main(void)
         }
         //printk(".\n");
     }
- 
 }
-
-
-/* UART callback implementation */
-/* Note that callback functions are executed in the scope of interrupt handlers. */
-/* They run asynchronously after hardware/software interrupts and have a higher priority than all threads */
-/* Should be kept as short and simple as possible. Heavier processing should be deferred to a task with suitable priority*/
-static void uart_cb(const struct device *dev, struct uart_event *evt, void *user_data)
-{
-    int err;
-
-    switch (evt->type) {
-	
-        case UART_TX_DONE:
-		    printk("UART_TX_DONE event \n\r");
-            k_sem_give(&uart_sem);
-            break;
-
-    	case UART_TX_ABORTED:
-	    	printk("UART_TX_ABORTED event \n\r");
-		    break;
-		
-	    case UART_RX_RDY:
-		    // printk("UART_RX_RDY event \n\r");
-            /* Just copy data to a buffer. Usually it is preferable to use e.g. a FIFO to communicate with a task that shall process the messages*/
-            memcpy(&rx_chars[uart_rxbuf_nchar],&(rx_buf[evt->data.rx.offset]),evt->data.rx.len);
-            uart_rxbuf_nchar++;
-		    break;
-
-	    case UART_RX_BUF_REQUEST:
-		    printk("UART_RX_BUF_REQUEST event \n\r");
-		    break;
-
-	    case UART_RX_BUF_RELEASED:
-		    printk("UART_RX_BUF_RELEASED event \n\r");
-		    break;
-		
-	    case UART_RX_DISABLED: 
-            /* When the RX_BUFF becomes full RX is is disabled automaticaly.  */
-            /* It must be re-enabled manually for continuous reception */
-            printk("UART_RX_DISABLED event \n\r");
-		    err =  uart_rx_enable(uart_dev ,rx_buf,sizeof(rx_buf),RX_TIMEOUT);
-            if (err) {
-                printk("uart_rx_enable() error. Error code:%d\n\r",err);
-                exit(FATAL_ERR);                
-            }
-		    break;
-
-	    case UART_RX_STOPPED:
-		    printk("UART_RX_STOPPED event \n\r");
-		    break;
-		
-	    default:
-            printk("UART: unknown event \n\r");
-		    break;
-    }
-
-}
+void LEDS_code(void *argA, void *argB, void *argC){}
+void BTNS_code(void *argA, void *argB, void *argC){}
+void TC74_code(void *argA, void *argB, void *argC){}
